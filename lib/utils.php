c225048fbb9fca9c04444766517d5ed2d8adc6e0
Michelle Schulp <michelle@marktimemedia.com>
2017-08-12 11:32:08 -0500

    style fixes and better taxonomy util

--- lib/utils.php
+++ lib/utils.php
@@ -13,11 +13,12 @@ function is_element_empty( $element ) {
   return empty( $element ) ? false : true;
 }
 
-/** 
+/**
 * Custom Taxonomy Term Links
 * From http://codex.wordpress.org/Function_Reference/get_the_terms
 * echo custom_taxonomies_terms_links(); in your template file
 */
+
 // get taxonomies terms links
 function custom_taxonomies_terms_links(){
   // get post by post id
@@ -32,19 +33,22 @@ function custom_taxonomies_terms_links(){
   $out = array();
   foreach ( $taxonomies as $taxonomy_slug => $taxonomy ){
 
-    // get the terms related to post
-    $terms = get_the_terms( $post->ID, $taxonomy_slug );
+    if ( 1 == $taxonomy->public ) {
 
-    if ( !empty( $terms ) ) {
-      $out[] = '<span class="post--metadata--title">' . $taxonomy->label . ': </span><ul>';
-      foreach ( $terms as $term ) {
-        $out[] =
-          '  <li><a href="'
-        .    get_term_link( $term->slug, $taxonomy_slug ) .'">'
-        .    $term->name
-        . "</a></li>\n";
+      // get the terms related to post
+      $terms = get_the_terms( $post->ID, $taxonomy_slug );
+
+      if ( !empty( $terms ) ) {
+        $out[] = '<div class="post--metadata-group"><span class="post--metadata--title">' . $taxonomy->label . ': </span><ul>';
+        foreach ( $terms as $term ) {
+          $out[] =
+            '  <li><a href="'
+          .    get_term_link( $term->slug, $taxonomy_slug ) .'">'
+          .    $term->name
+          . "</a></li>\n";
+        }
+        $out[] = "</ul></div>\n";
       }
-      $out[] = "</ul>\n";
     }
   }
 
